9440543913adb352c80ae4a600cba02bd837b362
Verky Yi <verky.yi@gmail.com>
2023-11-27 01:04:41 -0500

    label_classifier update

--- label_classifier/classifier_co_faculty.py
+++ label_classifier/classifier_co_faculty.py
@@ -1,8 +1,10 @@
-LABEL = 'TESTLABEL1'
+LABEL = 'CO_FACULTY'
 
 def classify_email(role, email):
     """Classify an email based on its content and the role of the user.
     role: the role of the user
     email: the email to be classified, which is an object.
     """
-    return True
+    if role == 'professor':
+        return True
+    return False
--- label_classifier/classifier_events.py
+++ label_classifier/classifier_events.py
@@ -1,11 +1,8 @@
-LABEL = 'TESTLABEL2'
+LABEL = 'EVENTS'
 
 def classify_email(role, email):
     """Classify an email based on its content and the role of the user.
     role: the role of the user
     email: the email to be classified, which is a panda row.
     """
-    # If Subject contains 'Treats', classify as 'TESTLABEL2'
-    if 'Treats'.lower() in email['subject'].lower():
-        return True
     return False
