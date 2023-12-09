6839bae70a21d2a55d6f0b20d0a2b40d903c48bf
jackyzhang1008 <jz922@georgetown.edu>
2023-12-09 17:20:36 -0500

    push

--- role_classifier/role_classifier.py
+++ role_classifier/role_classifier.py
@@ -12,13 +12,11 @@ def classify_role(mail_history:pd.DataFrame)->str:
   # If there is one mail existing in the mail hisotry from canvas, the content contains
   # 'Your assignment .* has been graded.', then the user is a student.
   cofaculty_Email = ['Purna.Gamage@georgetown.edu','heather.connor@georgetown.edu','qh86@georgetown.edu','jh2343@georgetown.edu','jj1088@georgetown.edu','ashley.stowe@georgetown.edu','np617@georgetown.edu','tva7@georgetown.edu']
-
   cofaculty_Name = ['Amit Arora','Abhijit Dasgupta','Benjamin Houghton','Chris Larson','Anderson Monken','Nate Strawn','Marck Vaisman','Irina Vayndiner','Nima Zahadat']
 
   for length in range(len(mail_history)):
-      if mail_history['account_name'].iloc[length] in cofaculty_Name or mail_history['account_mail_address'].iloc[length] in cofaculty_Email:
-          mail_history['account_role'].iloc[length] = 'professor'
-      else:
-          mail_history['account_role'].iloc[length] = 'student'
-  
+    if mail_history['account_name'].iloc[length] in cofaculty_Name or mail_history['account_mail_address'].iloc[length] in cofaculty_Email:
+      mail_history['account_role'].iloc[length] = 'professor'
+    else:
+      mail_history['account_role'].iloc[length] = 'student'
   return mail_history
