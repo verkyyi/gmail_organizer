ed64a2a2306983022076e0a2608d45a3e59e189f
DinoHan <hanzeyu89@gmail.com>
2023-12-09 15:14:02 -0500

    label creation

--- label_creation/label_office_department.py
+++ label_creation/label_office_department.py
@@ -1,17 +1,14 @@
-
 import pandas as pd
 '''
 A function which judge whether a label should be assigned to a user.
 It takes in a role and mail history and returns a boolean.
 '''
-LABEL = 'HR'
+LABEL = 'Georgetown Office Department'
 
 def assign(role:str, mail_history:pd.DataFrame)->bool:
   '''
   @param role: the role of the user
   @param mail_history: the mail history of the user
   @return: a boolean
-  @note: we could not find a way to distinguish the goal of students, so we may leave it to the user to decide.
   '''
-  if (role == 'professor'): return True
-  return False
+  return True
